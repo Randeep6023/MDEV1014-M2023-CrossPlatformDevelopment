6ec2e07a4850d02547224a91fb1baf06d504d8d4
anujchaudhari80 <200558521@student.georgianc.on.ca>
2023-07-06 15:19:45 -0400

    Login page style updated

--- Assignment1/Login.tsx
+++ Assignment1/Login.tsx
@@ -1,63 +1,88 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, Image, } from 'react-native';
 
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
   const handleLogin = () => {
-    // TODO: Perform login logic here.
+    // TODO: Perform login logic here
   };
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Login</Text>
-      <TextInput
-        placeholder="Email"
-        style={styles.input}
-        value={email}
-        onChangeText={setEmail}
-      />
-      <TextInput
-        placeholder="Password"
-        style={styles.input}
-        value={password}
-        onChangeText={setPassword}
-      />
-      <TouchableOpacity style={styles.button} onPress={handleLogin}>
-        <Text style={styles.buttonText}>Login</Text>
-      </TouchableOpacity>
+      {/*<Image style={styles.image} source={require('./assets/cartIcon.png')} />*/}
+      <View style={styles.subContainer}>
+        <Text style={styles.logo}>OneStopShop</Text>
+        <View style={styles.inputView}>
+          <TextInput
+            style={styles.inputText}
+            placeholder="Email..."
+            placeholderTextColor="#000000"
+            onChangeText={(text) => setEmail(text)}
+          />
+        </View>
+        <View style={styles.inputView}>
+          <TextInput
+            secureTextEntry
+            style={styles.inputText}
+            placeholder="Password..."
+            placeholderTextColor="#000000"
+            onChangeText={(text) => setPassword(text)}
+          />
+        </View>
+        <TouchableOpacity style={styles.loginBtn} onPress={handleLogin}>
+          <Text style={styles.loginText}>LOGIN</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
 
-    const styles = StyleSheet.create({
-        container: {
-          flex: 1,
-          alignItems: 'center',
-          justifyContent: 'center',
-          backgroundColor: '#66BB6A',
-        },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#66BB6A',
   },
-  input: {
-    height: 40,
-    borderColor: '#ccc',
-    borderWidth: 1,
-    padding: 10,
+  image: {
+    flex: 1,
+    resizeMode: 'cover',
   },
-  button: {
-    backgroundColor: '#FFFFFF',
+  subContainer: {
+    ...StyleSheet.absoluteFillObject,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logo: {
+    fontSize: 50,
     color: '#fff',
-    height: 40,
-    borderRadius: 5,
-    padding: 10,
+    marginBottom: 40,
   },
-  buttonText: {
-    fontSize: 16,
-    fontWeight: 'bold',
+  inputView: {
+    width: '80%',
+    backgroundColor: '#fff',
+    borderRadius: 25,
+    height: 50,
+    marginBottom: 20,
+    justifyContent: 'center',
+    padding: 20,
+  },
+  inputText: {
+    height: 50,
+    color: '#009688',
+  },
+  loginBtn: {
+    width: '80%',
+    backgroundColor: '#fff',
+    borderRadius: 25,
+    height: 50,
+    marginBottom: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  loginText: {
+    color: '#009688',
+    fontSize: 18,
   },
 });
 
