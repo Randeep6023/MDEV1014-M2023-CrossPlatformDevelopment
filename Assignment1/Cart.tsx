ee5962a985f9eb8877df2736d7a592865a7dfdb7
Ilham Sheikh <ilhamsheikh@Ilhams-MacBook-Pro.local>
2023-07-11 18:06:51 -0400

    Added Images, Layouts, styling to the cart screen (Ilham)

--- Assignment1/Cart.tsx
+++ Assignment1/Cart.tsx
@@ -12,14 +12,15 @@
 // export default Cart;
 
 import React from 'react';
-import { View, Text, Button, StyleSheet, FlatList } from 'react-native';
+import { View, Text, Button, StyleSheet, FlatList, Touchable, Image } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 const CartScreen = () => {
   // Sample cart items
   const cartItems = [
-    { id: 1, name: 'Product 1', price: 10 },
-    { id: 2, name: 'Product 2', price: 20 },
-    { id: 3, name: 'Product 3', price: 15 }
+    { id: 1, name: 'Coffee Table', price: 100, quantity: 1, image: require('./assets/table1.jpeg')},
+    { id: 2, name: 'Comb', price: 20, quantity: 1, image: require('./assets/comb.png')},
+    { id: 3, name: 'Toy', price: 15, quantity: 1, image: require('./assets/toy.png')}
   ];
 
   // Function to handle checkout button press
@@ -28,7 +29,7 @@ const CartScreen = () => {
     alert('Checkout button pressed!');
   };
 
-  return (
+   return (
     <View style={styles.container}>
       {/* <View style={styles.header}>
         <Text style={styles.headerText}>Shopping Cart</Text>
@@ -40,13 +41,49 @@ const CartScreen = () => {
           keyExtractor={(item) => item.id.toString()}
           renderItem={({ item }) => (
             <View style={styles.cartItem}>
-              <Text style={styles.name}>{item.name}</Text>
-              <Text style={styles.price}>${item.price}</Text>
+              <View style = {styles.imageContainer}>
+                <Image source = {item.image} style = {styles.itemImage} />
+              </View>
+              
+              <View style={styles.itemInfo}>
+                <Text style={styles.name}>{item.name}</Text>
+                <Text style={styles.quantity}>Quantity: {item.quantity}</Text>
+              </View>
+
+              <View style={styles.priceContainer}>
+                <Text style={styles.price}>${item.price}</Text>
+                <View style={styles.deleteButton}>
+                  <Text style = {styles.deleteButtonText}>Delete</Text>
+                </View>
+              </View>
+             
+            </View>
+          )}
+          ItemSeparatorComponent={() => <View style={styles.separator} />}
+          ListFooterComponent={() => (
+            <View style={styles.footer}>
+              <View style = {styles.eachRow}>
+                <Text style={styles.totalText}>Total Price: </Text>
+                <Text style = {styles.rowValue}>$135</Text>
+              </View>
+              <View style = {styles.eachRow}>
+                <Text style={styles.deliveryCharge}>Delivery Charge: </Text>
+                <Text style = {styles.rowValue}>$10</Text>
+              </View>
+              <View style = {styles.eachRow}>
+                <Text style={styles.finalPrice}>Final Price: </Text>
+                <Text style = {styles.finalPriceValue}>$145</Text>
+              </View>
+              <View style = {styles.eachRow}>
+                <Text style={styles.etaText}>ETA: 2-3 Business days </Text>
+              </View>
+              
             </View>
           )}
         />
-
-        <Button title="Checkout" onPress={handleCheckout} />
+      </View>
+      <View style={styles.bottomButton}>
+        <Text style = {styles.buttonText}>Proceed to Pay</Text>
       </View>
     </View>
   );
@@ -71,14 +108,102 @@ const styles = StyleSheet.create({
   },
   cartItem: {
     flexDirection: 'row',
-    justifyContent: 'space-between',
+    alignItems: 'center',
     marginBottom: 10,
+  },
+  itemInfo: {
+    flexDirection: 'column',
+    alignItems: 'flex-start',
+    marginLeft : 10,
+    flex: 1
   },
   name: {
     fontWeight: 'bold',
+    marginRight: 10,
+    fontSize : 20
+  },
+  imageContainer : {
+
+  },
+  itemImage : {
+    width : 60,
+    height : 60,
+    resizeMode : "cover",
   },
   price: {
-    color: 'green',
+    color: 'black',
+    fontWeight : "700",
+    alignSelf : "center"
+  },
+  priceContainer: {
+    flexDirection: 'column',
+    alignItems: 'flex-start',
+  },
+  quantity: {
+    fontSize: 16,
+  },
+  separator: {
+    height: 1,
+    backgroundColor: 'white',
+    marginVertical: 10,
+  },
+  footer: {
+    marginTop: 20,
+  },
+  totalText: {
+    fontSize: 17,
+    marginBottom: 10,
+    flex: 1
+  },
+  rowValue: {
+    fontSize: 17,
+    marginBottom: 10,
+  },
+  deliveryCharge: {
+    fontSize: 17,
+    marginBottom: 10,
+    flex: 1
+  },
+  finalPrice: {
+    fontWeight: 'bold',
+    fontSize: 26,
+    marginBottom: 10,
+    flex: 1
+  },
+  finalPriceValue: {
+    fontWeight: 'bold',
+    fontSize: 26,
+    marginBottom: 10,
+  },
+  etaText: {
+    fontSize: 17,
+    marginBottom: 5,
+  },
+  eachRow: {
+    flexDirection: "row",
+  },
+  bottomButton: {
+    backgroundColor: 'black',
+    marginBottom: 18,
+    height: 55,
+    flexDirection: "row",
+    justifyContent: 'center',
+    alignItems: "center"
+  },
+  buttonText: {
+    color : "white",
+    fontWeight : "bold",
+    fontSize : 20
+  },
+  deleteButton: {
+    backgroundColor: 'black',
+    borderRadius: 5,
+    marginTop : 5,
+    height : 30
+  },
+  deleteButtonText: {
+    color: 'white',
+    margin : 5
   },
 });
 
